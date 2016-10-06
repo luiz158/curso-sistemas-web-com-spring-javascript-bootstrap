3b654445b24020211441f7add4197d06cee20c25
Luiz Augusto <luiz158@gmail.com>
2016-10-06 17:24:03 -0300

    mudancas para postgresql no lugar do mysql...

--- 15.19-corrigindo-bug-na-pesquisa/brewer/src/main/resources/db/migration/V01__criar_tabelas_estilo_e_cerveja.sql
+++ 15.19-corrigindo-bug-na-pesquisa/brewer/src/main/resources/db/migration/V01__criar_tabelas_estilo_e_cerveja.sql
@@ -1,11 +1,11 @@
 CREATE TABLE estilo (
-    codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    codigo serial PRIMARY KEY not null,
+    nome VARCHAR(50) NOT NULL unique
+);
 
 CREATE TABLE cerveja (
-    codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    sku VARCHAR(50) NOT NULL,
+    codigo serial PRIMARY KEY not null,
+    sku VARCHAR(50) NOT NULL unique,
     nome VARCHAR(80) NOT NULL,
     descricao TEXT NOT NULL,
     valor DECIMAL(10, 2) NOT NULL,
@@ -13,11 +13,13 @@ CREATE TABLE cerveja (
     comissao DECIMAL(10, 2) NOT NULL,
     sabor VARCHAR(50) NOT NULL,
     origem VARCHAR(50) NOT NULL,
-    codigo_estilo BIGINT(20) NOT NULL,
+    codigo_estilo integer NOT NULL,
     FOREIGN KEY (codigo_estilo) REFERENCES estilo(codigo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-INSERT INTO estilo VALUES (0, 'Amber Lager');
-INSERT INTO estilo VALUES (0, 'Dark Lager');
-INSERT INTO estilo VALUES (0, 'Pale Lager');
-INSERT INTO estilo VALUES (0, 'Pilsner');
+INSERT INTO estilo (nome) VALUES ('Amber Lager');
+INSERT INTO estilo (nome) VALUES ('Dark Lager');
+INSERT INTO estilo (nome) VALUES ('Pale Lager');
+INSERT INTO estilo (nome) VALUES ('Pilsner');
+
+
