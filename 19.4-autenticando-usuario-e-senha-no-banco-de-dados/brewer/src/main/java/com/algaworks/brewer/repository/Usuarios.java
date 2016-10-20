19cfaaa758e17088871090871148da74e2ea8702
Luiz Augusto <luiz158@gmail.com>
2016-10-20 12:10:50 -0200

    algumas coisitas a mais sobre security no spring - seguranca com banco de dados, csrf, teste de ataque...

--- 19.4-autenticando-usuario-e-senha-no-banco-de-dados/brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
+++ 19.4-autenticando-usuario-e-senha-no-banco-de-dados/brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,8 @@ import com.algaworks.brewer.repository.helper.usuario.UsuariosQueries;
 public interface Usuarios extends JpaRepository<Usuario, Long>, UsuariosQueries {
 
 	public Optional<Usuario> findByEmail(String email);
+
+	// isto para o capítulo: 19.6
+	public List<String> permissoes(Usuario usuario);
 
 }
--- 19.4-autenticando-usuario-e-senha-no-banco-de-dados/brewer/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ 19.4-autenticando-usuario-e-senha-no-banco-de-dados/brewer/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.algaworks.brewer.security;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +29,16 @@ public class AppUserDetailsService implements UserDetailsService {
 		Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
 		return new User(usuario.getEmail(), usuario.getSenha(), new HashSet<>());
+	}
+
+	// isto para o capítulo: 19.6
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+		List<String> permissoes = usuarios.permissoes(usuario);
+		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority("ROLE_" + p.toUpperCase())));
+
+		return authorities;
 	}
 
 }
